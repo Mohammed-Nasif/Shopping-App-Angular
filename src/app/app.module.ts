7037a13e5f72683e71df8fcdcf8c5c6c67ddd267
Mohammed Nasif <m.khaled100@ymail.com>
2022-10-16 17:38:37 +0200

    make it pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { LoaderInterceptor } from './interceptors/loader.interceptor';
 import { wishListReducer } from './store/wishlist/wishlist.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
 	declarations: [
@@ -64,6 +65,12 @@ import { environment } from '../environments/environment';
 		SharedModule,
 		StoreModule.forRoot({ wishList: wishListReducer }, {}),
 		StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+  ServiceWorkerModule.register('ngsw-worker.js', {
+    enabled: environment.production,
+    // Register the ServiceWorker as soon as the application is stable
+    // or after 30 seconds (whichever comes first).
+    registrationStrategy: 'registerWhenStable:30000'
+  }),
 	],
 	providers: [
 		ProductsService,
